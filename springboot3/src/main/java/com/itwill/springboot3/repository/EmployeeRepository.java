4ddd2a77e7c470be6a8196d17d0e5d3d0875ac17
ldj0905 <ejj095@gmail.com>
2024-07-24 13:33:44 +0900

    완료

--- springboot3/src/main/java/com/itwill/springboot3/repository/EmployeeRepository.java
+++ springboot3/src/main/java/com/itwill/springboot3/repository/EmployeeRepository.java
@@ -9,7 +9,7 @@ import com.itwill.springboot3.domain.Employee;
 public interface EmployeeRepository extends JpaRepository<Employee, Integer>{
 	// JPA query method 작성 방법:
 	// JAP 에서 약속된 키워드들을 메서드 이름을 (camel 표기법으로) 작성할 때 사용.
-	
+	// 메서드 이름을 (camel 표기법으로) 작성 .
 	// select * from employees where department_id = ?
 	List<Employee> findByDepartmentId(Integer id); // 메서드를 선언만 해준다. 
 	
